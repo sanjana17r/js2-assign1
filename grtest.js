c5a5a4ebbfeef386a83b260721aee79fffe39958
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2018-05-22 22:27:02 +0530

    Add files via upload

---
+++ grtest.js
@@ -0,0 +1,7 @@
+var a=-1, b=2, c=0,d=3,e=-2;//five variables are declared of different values
+grt1 = ((a>b)?a:b);//grt1 stores greatest of a and b
+grt2 = ((d>e)?d:e);//grt2 stores greatest of d and e
+grt3 = ((grt1>grt2)?grt1:grt2);//grt3 stores greatest of grt1 and grt2
+grt4 =((grt3>c)?grt3:c); //grt4 stores greatest of grt3 and c
+console.log(grt4); //hence grt4 stores greatest of all the five numbers
+//so,the greatest of all the numbers is console logged.
